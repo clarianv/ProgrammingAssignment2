9879f86f4ca83ba26865ce4401aa5a521a4e9674
clarianv <clarianv@yahoo.com>
2018-10-04 14:06:16 +1000

    use inverse instead of mean

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,7 @@
 #
 # Example:
 # d1 <- makeCacheMatrix(matrix(6:15,nrow=2)) -- here it creates the special matrix 
-# cacheSolve(d1) -- here it checks that there is no cached data yet, so it calculates the mean an save the value
+# cacheSolve(d1) -- here it checks that there is no cached data yet, so it calculates the inverse an save the value
 # cacheSolve(d1) -- here it checks that there is cached data, and just returns the previously saved value
 
 
@@ -14,8 +14,8 @@
 #creates a special "matrix", which is really a list containing a function to
 #1. set the value of the matrix
 #2. get the value of the matrix
-#3. set the value of the mean
-#4. get the value of the mean
+#3. set the value of the inverse
+#4. get the value of the inverse
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -23,25 +23,25 @@ makeCacheMatrix <- function(x = matrix()) {
     m <<- NULL
   }
   get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
   list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## gets the mean of a matrix by using the special matrix above, where
-#1. it tries to retreive a previously calculated mean, or if none
-#2. calculates the mean, then save it for possible future use
+## gets the inverse of a matrix by using the special matrix above, where
+#1. it tries to retreive a previously calculated inverse, or if none
+#2. calculates the inverse, then save it for possible future use
 cacheSolve <- function(x, ...) {
-  m <- x$getmean()
+  m <- x$getinverse()
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
   data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
+  m <- inverse(data, ...)
+  x$setinverse(m)
   m
 }
